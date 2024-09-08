edc0232bfd5ab269217ee5bb65d2f4c22ad44a5d
heonuk <gitshineit@gmail.com>
2024-09-08 12:16:22 +0900

    :construction_worker: build: 살구 서비스 초기 설정

---
+++ src/main/java/sandbox/apricot/ApricotBatchApplication.java
@@ -0,0 +1,13 @@
+package sandbox.apricot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApricotBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApricotBatchApplication.class, args);
+    }
+
+}
