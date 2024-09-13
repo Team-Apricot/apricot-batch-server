086103487bd8f70a5d6e36e6845f0819932ff115
ssoulistic <ssoulistic@gmail.com>
2024-09-13 14:51:03 +0900

    :truck: rename: AppConfig to RestTemplateConfig

--- src/main/java/sandbox/apricot/config/RestTemplateConfig.java
+++ src/main/java/sandbox/apricot/config/RestTemplateConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppConfig {
+public class RestTemplateConfig {
 
+  //RestTemplate
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
