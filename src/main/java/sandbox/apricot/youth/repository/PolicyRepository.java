074215558b54e4dcd2447b8607422ba54e4d3e62
heonuk <gitshineit@gmail.com>
2024-09-15 03:15:44 +0900

    :fire: remove: 사용하지 않는 코드 제거

--- src/main/java/sandbox/apricot/youth/repository/PolicyRepository.java
+++ src/main/java/sandbox/apricot/youth/repository/PolicyRepository.java
@@ -1,11 +1,7 @@
 package sandbox.apricot.youth.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sandbox.apricot.youth.entity.Policy;
 
 public interface PolicyRepository extends JpaRepository<Policy, String> {
-
-    Optional<Policy> findByPolicyCode(String policyCode);
-
 }
